472acde010c065aaf782937e240c512319f77845
koki-develop <kou.pg.0131@gmail.com>
2025-03-18 21:42:04 +0900

    fix(zizmor): Set the GH_TOKEN variable only if github-token is passed

--- dockerfiles/zizmor/entrypoint.sh
+++ dockerfiles/zizmor/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+if [ -n "${ZIZMOR_GITHUB_TOKEN}" ]; then
+  export GH_TOKEN="${ZIZMOR_GITHUB_TOKEN}"
+fi
+
 /app/zizmor . \
   --color=always \
   --persona="${ZIZMOR_PERSONA:-regular}"
