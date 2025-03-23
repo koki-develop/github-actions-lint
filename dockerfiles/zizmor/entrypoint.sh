355977221caabfff97ac7efc32de95210a70746d
koki-develop <kou.pg.0131@gmail.com>
2025-03-24 08:01:22 +0900

    feat(zizmor): Use reviewdog

--- dockerfiles/zizmor/entrypoint.sh
+++ dockerfiles/zizmor/entrypoint.sh
@@ -1,9 +1,23 @@
 #!/bin/sh
 
-if [ -n "${ZIZMOR_GITHUB_TOKEN}" ]; then
-  export GH_TOKEN="${ZIZMOR_GITHUB_TOKEN}"
+if [ -n "${GITHUB_TOKEN}" ]; then
+  export GH_TOKEN="${GITHUB_TOKEN}"
+  export REVIEWDOG_GITHUB_TOKEN="${GITHUB_TOKEN}"
 fi
 
-/app/zizmor . \
-  --color=always \
-  --persona="${ZIZMOR_PERSONA:-regular}"
+if [ "${REVIEWDOG_ENABLED}" = 'true' ]; then
+  /app/zizmor . \
+    --persona="${ZIZMOR_PERSONA:-regular}" \
+    --format=sarif \
+    | reviewdog \
+        -name=zizmor \
+        -f=sarif \
+        -reporter="${REVIEWDOG_REPORTER:-github-pr-review}" \
+        -level=error \
+        -filter-mode="${REVIEWDOG_FILTER_MODE:-added}" \
+        -fail-level="${REVIEWDOG_FAIL_LEVEL:-none}"
+else
+  /app/zizmor . \
+    --color=always \
+    --persona="${ZIZMOR_PERSONA:-regular}"
+fi
